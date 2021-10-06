a2880c484f19cafdf994f99f7be57172b90822a4
Deyan Grigorov <deian1998@abv.bg>
2021-10-06 23:32:15 +0300

    Add new problens

--- Dom-introduction/04. Search in List/search.js
+++ Dom-introduction/04. Search in List/search.js
@@ -1,3 +1,18 @@
-function search() {
-   // TODO
+function search () {
+  const towns = Array.from(document.querySelectorAll('ul li'));
+  const text = document.getElementById('searchText').value;
+  let matches = 0;
+
+  towns.forEach((el) => {
+    if (el.innerHTML.includes(text)) {
+      el.style.fontWeight = 'bold';
+      el.style.textDecoration = 'underline';
+      matches++;
+
+    } else {
+      el.style.fontWeight = 'normal';
+      el.style.textDecoration = '';
+    }
+  });
+  document.getElementById('result').textContent = `${matches} matches found`;
 }
--- Dom-introduction/05. Table - Search Engine/solution.js
+++ Dom-introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,21 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+function solve () {
+  document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick () {
+    const input = document.getElementById('searchField');
+    let inputText = input.value.toLowerCase();
 
-   }
+    let tableElements = Array.from(document.querySelectorAll('tbody tr'));
+
+    tableElements.forEach((el) => {
+      let text = el.textContent.toLowerCase();
+      if (text.includes(inputText)) {
+        el.classList.add('select');
+      } else {
+        el.classList.remove('select');
+      }
+    });
+    input.value = '';
+
+  }
 }
