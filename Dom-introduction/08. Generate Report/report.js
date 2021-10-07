5b45a623a8c7db1fd264aeb1aa50fcb36e987608
Deyan Grigorov <deian1998@abv.bg>
2021-10-07 18:57:01 +0300

    Add new exc.

--- Dom-introduction/08. Generate Report/report.js
+++ Dom-introduction/08. Generate Report/report.js
@@ -1,3 +1,28 @@
-function generateReport() {
-    //TODO
+function generateReport () {
+  let info = document.querySelectorAll('input');
+  let table = document.querySelectorAll('tbody tr');
+
+  let tableArray = new Array(table)[0];
+
+  let output = [];
+
+  for (let i = 0; i < table.length; i++) {
+    let employerInfo = {};
+    for (let k = 0; k < info.length; k++) {
+      if (info[k].checked === true) {
+        let label = info[k].name;
+        console.log(tableArray[i].textContent.split('\n').map(x => x.trim()));
+
+        employerInfo[label] = tableArray[i].textContent.split('\n').map(x => x.trim()).filter(x => x !== "")[k];
+      }
+    }
+    output.push(employerInfo);
+  }
+  console.log(output);
+  let outputResult = JSON.stringify(output);
+  let divOutput = document.getElementById('output');
+
+  divOutput.textContent = outputResult;
+
+
 }
